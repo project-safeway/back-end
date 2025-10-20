64f609eb274068190a1885aa714df73de50163c2
Th-Sanchez <thiago.peluci@sptech.school>
2025-10-20 19:38:54 -0300

    fix: correção da query de busca de pagamentos por usuário

--- src/main/java/com/safeway/tech/repository/PagamentoRepository.java
+++ src/main/java/com/safeway/tech/repository/PagamentoRepository.java
@@ -4,11 +4,13 @@ import com.safeway.tech.models.Funcionario;
 import com.safeway.tech.models.Pagamento;
 import com.safeway.tech.models.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface PagamentoRepository extends JpaRepository<Pagamento, Long> {
     List<Pagamento> findByFuncionario(Funcionario funcionario);
 
+    @Query("select p from Pagamento p where :usuario member of p.funcionario.transporte.usuarios")
     List<Pagamento> findPagamentosByUsuario(Usuario usuario);
 }
