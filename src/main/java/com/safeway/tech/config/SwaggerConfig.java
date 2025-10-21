dc70e1a1ac55cf5d73b2a1812e5222c9b2515d9e
jusley junior <jusley.nogueirajr@sptech.school>
2025-10-21 20:10:19 -0300

    doc:Swagger adição do SwaggerConfig

---
+++ src/main/java/com/safeway/tech/config/SwaggerConfig.java
@@ -0,0 +1,33 @@
+package com.safeway.tech.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        SecurityScheme securityScheme = new SecurityScheme()
+                .name("bearerAuth")
+                .type(SecurityScheme.Type.HTTP)
+                .scheme("bearer")
+                .bearerFormat("JWT")
+                .description("Insira o token JWT no formato: Bearer {token}");
+
+        SecurityRequirement securityRequirement = new SecurityRequirement().addList("bearerAuth");
+
+        return new OpenAPI()
+                .components(new Components().addSecuritySchemes("bearerAuth", securityScheme))
+                .addSecurityItem(securityRequirement)
+                .info(new Info()
+                        .title("API Safeway Tech")
+                        .description("Documentação da API com autenticação JWT")
+                        .version("1.0.0"));
+    }
+}
