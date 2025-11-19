34f407b7d9141c54ca983ed0c801735a547483e7
Guilherme-GotardoF3 <gsantos@f3capital.com.br>
2025-11-19 20:16:12 -0300

    fix: scheduler and event panel

--- src/main/java/com/safeway/tech/repository/MensalidadeRepository.java
+++ src/main/java/com/safeway/tech/repository/MensalidadeRepository.java
@@ -3,11 +3,8 @@ package com.safeway.tech.repository;
 import com.safeway.tech.enums.StatusPagamento;
 import com.safeway.tech.models.Aluno;
 import com.safeway.tech.models.MensalidadeAluno;
-import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -19,8 +16,7 @@ public interface MensalidadeRepository extends JpaRepository<MensalidadeAluno, L
             LocalDate data
     );
 
-    @Query("SELECT m FROM MensalidadeAluno m WHERE m.aluno = :aluno AND m.dataVencimento BETWEEN :dataInicio AND :dataFim")
-    Boolean existsByAlunoAndDataVencimentoBetween(
+    boolean existsByAlunoAndDataVencimentoBetween(
             Aluno aluno,
             LocalDate dataInicio,
             LocalDate dataFim);
