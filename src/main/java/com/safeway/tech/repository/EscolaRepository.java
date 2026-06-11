5c9c9c9ca46c1e2d75551740570d70693ef16ffe
Th-Sanchez <thibpeluci@gmail.com>
2026-06-10 22:03:28 -0300

    fix: filter to only get active schools

--- src/main/java/com/safeway/tech/repository/EscolaRepository.java
+++ src/main/java/com/safeway/tech/repository/EscolaRepository.java
@@ -10,9 +10,9 @@ import java.util.Optional;
 import java.util.UUID;
 
 public interface EscolaRepository extends JpaRepository<Escola, UUID> {
-    @Query("SELECT e FROM Escola e WHERE e.usuario.id = :usuarioId")
+    @Query("SELECT e FROM Escola e WHERE e.usuario.id = :usuarioId AND e.ativo = true")
     List<Escola> findByUsuarioIdUsuario(@Param("usuarioId") UUID usuarioId);
 
-    @Query("SELECT e FROM Escola e WHERE e.id = :idEscola AND e.usuario.id = :userId")
+    @Query("SELECT e FROM Escola e WHERE e.id = :idEscola AND e.usuario.id = :userId AND e.ativo = true")
     Optional<Escola> findByIdEscolaAndIdUsuario(@Param("idEscola") UUID idEscola, @Param("userId") UUID userId);
 }
