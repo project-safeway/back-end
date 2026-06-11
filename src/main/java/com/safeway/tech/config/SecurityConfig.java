1020d01f809296f010f636782eb174b5ef38ccc3
Th-Sanchez <thibpeluci@gmail.com>
2026-06-10 22:03:01 -0300

    fix: add react preview url to cors configuration

--- src/main/java/com/safeway/tech/config/SecurityConfig.java
+++ src/main/java/com/safeway/tech/config/SecurityConfig.java
@@ -16,6 +16,7 @@ import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.converter.RsaKeyConverters;
+import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.core.io.ClassPathResource;
 
@@ -69,8 +70,8 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         return request -> {
-            var cors = new org.springframework.web.cors.CorsConfiguration();
-            cors.setAllowedOrigins(List.of("http://localhost:5173", "http://localhost:3000"));
+            var cors = new CorsConfiguration();
+            cors.setAllowedOrigins(List.of("http://localhost:5173", "http://localhost:4173", "http://localhost:3000"));
             cors.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
             cors.setAllowedHeaders(List.of("*") );
             cors.setExposedHeaders(List.of("Authorization"));
